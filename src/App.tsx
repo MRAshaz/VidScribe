0ca13564759a0c9463787d6b0fe093d431b1a14f
Ashaz Akhtar <ashazsyed105@gmail.com>
2026-04-15 22:10:42 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Home from "./components/Home"
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css"
 
 function App() {
     return (
+        <>
         <Home/>
+        <Analytics/>
+        </>
     )
     
 }
